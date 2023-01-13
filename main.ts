f5599b1965506c809584fc9e6da136b69a3b2af9
gabe474 <122543660+gabe474@users.noreply.github.com>
2023-01-13 09:42:37 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Ghost)
+    music.playMelody("D F - D B E C F ", 120)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Target)
